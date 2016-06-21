8ba1fab498f4669a8e37c2116900e6f29fe69a35
kwkoizumi <koizumi@knowledge-works.co.jp>
2016-06-21 10:57:20 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+base=${0##*/}
+
+#echo -e "S3_MOUNT_NAME='mount'\nS3_BUCKET_NAME='bucket'" > /etc/goofys.conf
+[ -e /etc/goofys.conf ] && . /etc/goofys.conf
+
+[ "${S3_BUCKET_NAME}" == "" ] && logger -ist "${base}" "S3_BUCKET_NAME is empty." && exit 1
+
+[ "${S3_MOUNT_NAME}" ] && logger -ist "${base}" "S3_MOUNT_NAME is empty." && exit 1
+
+yum update -y
+yum install golang fuse git -y
+export GOPATH=$HOME/go
+go get github.com/kahing/goofys
+go install github.com/kahing/goofys
+
+[ -e ${S3_MOUNT_NAME} ] || mkdir -p ${S3_MOUNT_NAME}
+#${GOPATH}/bin/goofys ${S3_BUCKET_NAME} ${S3_MOUNT_NAME}
+echo "${GOPATH}/bin/goofys ${S3_BUCKET_NAME} ${S3_MOUNT_NAME}"
